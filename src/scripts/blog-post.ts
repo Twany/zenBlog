ac5d4d3f933ed781bd012b51917aa3dee81cafb9
Twany <1029134285@qq.com>
2026-07-01 14:23:29 +0800

    style: refine article reading layout

--- src/scripts/blog-post.ts
+++ src/scripts/blog-post.ts
@@ -53,6 +53,7 @@ tocLinks.forEach((link) => {
 if (tocToggle && tocPanel && tocIcon) {
   tocToggle.addEventListener('click', () => {
     const isOpen = tocPanel.classList.toggle('hidden') === false;
+    tocToggle.setAttribute('aria-expanded', String(isOpen));
     tocIcon.innerHTML = isOpen
       ? '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6" /></svg>'
       : '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6" /></svg>';
